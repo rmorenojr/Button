34a8f61556e5eef12cd646d2ddeec5e89225c635
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-09-14 00:07:08 -0600

    Updated Callbacks
    Correct filename, revised instatiation line and object names

--- Examples/Callbacks/Callbacks.ino
+++ Examples/Callbacks/Callbacks.ino
@@ -1,12 +1,17 @@
 /*
 ||
-|| @file ChangeState.ino
-|| @version 1.0
+|| @file Callbacks.ino
+|| @version 1.1
 || @author Ted Hayes
 || @contact ted.hayes@liminastudio.com
 ||
 || @description
 || | Demonstrates the use of asynchronous callback functions instead of synchronous state checks
+|| #
+||
+|| @revisions
+|| | 09_13_2018 v1.1 Correct file name above and revised instantiation
+|| |                 object name and format by Ricardo Moreno 
 || #
 ||
 || @license
@@ -17,18 +22,18 @@
 
 #include <Button.h>
 
-//create two Button objects for two physical switches
 /*
 || Wiring:
-|| GND -----/ ------ pin 12
-|| GND -----/ ------ pin 11
+|| GND -----/ ------ pin 11 to button1
+|| GND -----/ ------ pin 12 to button2
+|| GND -----/ ------ pin 13 to LED
 */
-
+//create two Button objects for two physical switches
 // Uses debounce mode. You may need to experiment with the debounce duration
 // for your particular switch.
 
-Button myButton = Button(12, BUTTON_PULLUP_INTERNAL, true, 50);
-Button myButton2 = Button(11, BUTTON_PULLUP_INTERNAL, true, 50);
+Button myButton1(12, BUTTON_PULLUP_INTERNAL, true, 50);
+Button myButton2(11, BUTTON_PULLUP_INTERNAL, true, 50);
 
 // Callbacks
 // "Button& b" is a reference to the button that triggered the callback
@@ -53,12 +58,12 @@ void onHold(Button& b){
 void setup(){
   Serial.begin(9600);
   Serial.println("Started");
-  pinMode(13,OUTPUT); //debug to led 13
+  pinMode(13,OUTPUT);              //debug to led 13
 
   // Assign callback functions
-  myButton.pressHandler(onPress);
-  myButton.releaseHandler(onRelease);
-  myButton.holdHandler(onHold, 1000); // must be held for at least 1000 ms to trigger
+  myButton1.pressHandler(onPress);
+  myButton1.releaseHandler(onRelease);
+  myButton1.holdHandler(onHold, 1000); // must be held for at least 1000 ms to trigger
 
   myButton2.pressHandler(onPress);
   myButton2.releaseHandler(onRelease);
@@ -67,6 +72,6 @@ void setup(){
 
 void loop(){
   // update the buttons' internals
-  myButton.process();
+  myButton1.process();
   myButton2.process();
 }
