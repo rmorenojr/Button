27b6c1ec8cc11d20b86b85ecf39effedecd54335
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-09-15 22:27:33 -0600

    Revised example Button.ino

--- Examples/Button/Button.ino
+++ Examples/Button/Button.ino
@@ -1,4 +1,4 @@
-/*
+/* ***********************************************************
 ||
 || @file Button.ino
 || @version 1.2
@@ -6,12 +6,14 @@
 || @contact alexanderbrevig@gmail.com
 ||
 || @description
-|| | Display the intuitive way of using a button when using this Hardware Abstraction class
+|| | Display the intuitive way of using a button when using this
+|| | Hardware Abstraction class
 || #
 ||
 || @revisions
-|| | xx_xx_xxxx Minor fixes by Ted Hayes / ted.hayes@liminastudio.com
-|| | 09_13_2018 v1.2 Revised instantiation object name and format by Ricardo Moreno
+|| | xx_xx_xxxx v1.1 Minor fixes by Ted Hayes / ted.hayes@liminastudio.com
+|| | 09_13_2018 v1.2 Revised instantiation object name and format,
+|| |                 by Ricardo Moreno
 || #
 ||
 || @license
@@ -20,20 +22,22 @@
 || | alphabeta.alexanderbrevig.com/AlphaLicense.txt
 || #
 ||
-*/
+ * ********************************************************* */
 
 #include <Button.h>
 
-/*
-|| Wiring:
-|| GND -----/ ------ pin 12 to button
-|| GND -----/ ------ pin 13 to LED
-*/
-//create a Button object at pin 12
-Button myButton(12, BUTTON_PULLUP_INTERNAL);
+/* ***********************************************************
+|| Circuit Wiring:
+|| GND -----/ ------ pin 11 to button
+|| GND -----/ ------ pin 13 to LED (on board LED)
+||
+ * ********************************************************* */
+
+//create a Button object at pin 11
+Button myButton(11, BUTTON_PULLUP_INTERNAL);
 
 void setup(){
-  pinMode(13,OUTPUT);         //debug to led 13
+  pinMode(13,OUTPUT);         //led on pin 13
 }
 
 void loop(){
