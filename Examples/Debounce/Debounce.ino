d407e1b786136164030fe523c28b59f3cd62f26e
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-09-15 22:25:41 -0600

    Revised example Debounce.ino

--- Examples/Debounce/Debounce.ino
+++ Examples/Debounce/Debounce.ino
@@ -1,7 +1,7 @@
-/*
+/* ***********************************************************
 ||
 || @file Debounce.ino
-|| @version 1.1
+|| @version 1.2
 || @author Ted Hayes
 || @contact ted.hayes@liminastudio.com
 ||
@@ -10,6 +10,9 @@
 || #
 ||
 || @revisions
+|| | 09_15_2018 v1.2 Revised Wiring, LED 13 used by serial monitor,
+|| |                 revised digitalWrite statement in void loop to toggle
+|| |                 by Ricardo Moreno
 || | 09_13_2018 v1.1 Correct file name above and revised instantiation
 || |                 object name and format by Ricardo Moreno 
 || #
@@ -18,31 +21,31 @@
 || | Copyright (c) 2012 Ted Hayes. All rights reserved.
 || #
 ||
-*/
+ * ********************************************************* */
 
 #include <Button.h>
 
-/*
-|| Wiring:
-|| GND -----/ ------ pin 12 to button
-|| GND -----/ ------ pin 13 to LED
-*/
-//create a Button object at pin 12
+/* ***********************************************************
+|| Circuit Wiring:
+|| GND -----/ ------ pin 11 to button
+|| GND -----/ ------ pin 12 to LED
+||
+ * ********************************************************* */
+
+//create a Button object at pin 11
 // Uses debounce mode. You may need to experiment with the debounce duration
 // for your particular switch.
 
-Button myButton(12, BUTTON_PULLUP_INTERNAL, true, 50);
+Button myButton(11, BUTTON_PULLUP_INTERNAL, true, 50);
 
 void setup(){
   Serial.begin(9600);
-  pinMode(13,OUTPUT); //debug to led 13
+  pinMode(12,OUTPUT); //debug to led 12
 }
 
 void loop(){
   if(myButton.uniquePress()){
     Serial.println("Button Pressed");
-    digitalWrite(13,HIGH);
-  }else{
-    digitalWrite(13,LOW);
+    digitalWrite(12, !digitalRead(12)); 
   }
 }
