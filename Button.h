3093ae8cb02c513daa79826735bea280f681ebd0
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-09-15 22:31:04 -0600

    Minor revisions to Button.h
    Removed property pin.  Rather, added new int pinValue() method to get pin value.  Made holdThreshold and doubleclickThreshold properties public. New isDoubleClicked method and Callback doubleclickHander.

--- Button.h
+++ Button.h
@@ -9,7 +9,11 @@
 || | This is a Hardware Abstraction Library for Buttons
 || | It providea an easy way of handling buttons
 || |
-|| | Added doubl-click method by Ricardo Moreno https://github.com/rmorenojr/Button
+|| #
+||
+|| @revisions
+|| | 09_14_2008 v1.1 Added double-click callback method 
+|| |                 by Ricardo Moreno https://github.com/rmorenojr/Button
 || #
 ||
 || @license LICENSE_REPLACE
@@ -33,33 +37,40 @@ class Button {
   
     Button(uint8_t buttonPin, uint8_t buttonMode=BUTTON_PULLUP_INTERNAL, bool _debounceMode=true, int _debounceDuration=20);
 
-    uint8_t             pin;
-    
+    //Public variables
+    unsigned int        holdEventThreshold;             //uint16_t
+    unsigned int        doubleclickThreshold;
+
+    //Public methods
     void pullup(uint8_t buttonMode);
     void pulldown();
     void process();
 
     bool isPressed(bool proc=true);
+    bool isDoubleClicked(bool proc=true);
     bool wasPressed(bool proc=true);
     bool stateChanged(bool proc=true);
     bool uniquePress();
     
     void setHoldThreshold(unsigned int holdTime);
+    void setdoubleclickThreshold(unsigned int doublclickTime);  //The time in ms when first click starts and second click ends
     bool held(unsigned int time=0);
     bool heldFor(unsigned int time);
     
     void pressHandler(buttonEventHandler handler);
     void releaseHandler(buttonEventHandler handler);
     void clickHandler(buttonEventHandler handler);
-    void doubleclickHandler(buttonEventHandler handler, unsigned int doublclickTime=600);
+    void doubleclickHandler(buttonEventHandler handler, unsigned int doublclickTime=900);
     void holdHandler(buttonEventHandler handler, unsigned int holdTime=0);
   
     unsigned int holdTime() const;
     inline unsigned int presses() const { return numberOfPresses; }
+    inline unsigned int pinValue() const { return pin; }
     
     bool operator==(Button &rhs);
     
   private: 
+    uint8_t             pin;
     uint8_t             mode;
     uint8_t             state;
     bool                debounceMode;
@@ -67,8 +78,6 @@ class Button {
     unsigned long       pressedStartTime;
     unsigned long       previouspressedStartTime;
     unsigned long       releasedTime;
-    unsigned int        holdEventThreshold;
-    unsigned int        doubleclickThreshhold;
     unsigned long       debounceStartTime;
     int                 debounceDuration;
     buttonEventHandler  cb_onPress;
