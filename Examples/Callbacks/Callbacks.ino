e02d1af69460aceca05d97d9d42b83dd844b229f
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-09-15 22:27:19 -0600

    Revised example Callbacks.ino
    Included the new doubleclickHandler callback method.  Simple isDoubleClicked method has its own example.

--- Examples/Callbacks/Callbacks.ino
+++ Examples/Callbacks/Callbacks.ino
@@ -1,15 +1,20 @@
-/*
+/* ***********************************************************
 ||
 || @file Callbacks.ino
-|| @version 1.1
+|| @version 1.2
 || @author Ted Hayes
 || @contact ted.hayes@liminastudio.com
 ||
 || @description
-|| | Demonstrates the use of asynchronous callback functions instead of synchronous state checks
+|| | Demonstrates the use of asynchronous callback functions 
+|| | instead of synchronous state checks
 || #
 ||
 || @revisions
+|| | 09_15_2018 v1.2 Revised Wiring, made pin values constants,
+|| |                 revised button object names, revised callback
+|| |                 functions, included Callback doubleclickHandler
+|| |                 by Ricardo Moreno
 || | 09_13_2018 v1.1 Correct file name above and revised instantiation
 || |                 object name and format by Ricardo Moreno 
 || #
@@ -18,60 +23,96 @@
 || | Copyright (c) 2012 Ted Hayes. All rights reserved.
 || #
 ||
-*/
+ * ********************************************************* */
 
 #include <Button.h>
 
-/*
-|| Wiring:
-|| GND -----/ ------ pin 11 to button1
-|| GND -----/ ------ pin 12 to button2
-|| GND -----/ ------ pin 13 to LED
-*/
+/* ***********************************************************
+|| Circuit Wiring:
+|| GND -----/ ------ pin 9 Button1
+|| GND -----/ ------ pin 10 Button2
+|| GND -----/ ------ pin 11 LED
+|| GND -----/ ------ pin 12 LED
+||
+ * ********************************************************* */
+
+/* ***********************************************************
+ *                      Global Constants                     *
+ * ********************************************************* */
+const int b1Pin = 9;
+const int b2Pin = 10;
+const int led1Pin = 11;
+const int led2Pin = 12;
+
 //create two Button objects for two physical switches
 // Uses debounce mode. You may need to experiment with the debounce duration
 // for your particular switch.
 
-Button myButton1(12, BUTTON_PULLUP_INTERNAL, true, 50);
-Button myButton2(11, BUTTON_PULLUP_INTERNAL, true, 50);
+Button ltButton(b1Pin, BUTTON_PULLUP_INTERNAL, true, 50);
+Button rtButton(b2Pin, BUTTON_PULLUP_INTERNAL, true, 50);
 
-// Callbacks
-// "Button& b" is a reference to the button that triggered the callback
-// This way we can access its information so we know which button it was
+
+/* ***********************************************************
+ * Callback functions                                        *
+ *   "Button& b" is a reference to the button that triggered *
+ *   the callback. This way we can access its information so *
+ *   we know which button it was.                            *
+ * ********************************************************* */
+
 void onPress(Button& b){
-	Serial.print("onPress: ");
-	Serial.println(b.pin);
-	digitalWrite(13,HIGH);
+  Serial.print("onPress on pin: "); Serial.println(b.pinValue());
 }
 
 void onRelease(Button& b){
-	Serial.print("onRelease: ");
-	Serial.println(b.pin);
-	digitalWrite(13,LOW);
+  Serial.print("onRelease on pin: "); Serial.println(b.pinValue());
 }
 
 void onHold(Button& b){
-	Serial.print("Held for at least 1 second: ");
-	Serial.println(b.pin);
+  Serial.print("Held for at least 1 second: "); Serial.println(b.pinValue());
+  Serial.print("  holdEventThreshold is: "); Serial.print(b.holdEventThreshold);  Serial.println(" ms");
+
+  //we only want change one of the LEDs that corresponds to that button
+  if(b.pinValue()==b1Pin){ digitalWrite(led1Pin, !digitalRead(led1Pin)); }
+  if(b.pinValue()==b2Pin){ digitalWrite(led2Pin, !digitalRead(led2Pin)); }
 }
 
+void onDoubleClick(Button& b){
+  Serial.print("Double-click on pin: ");  Serial.println(b.pinValue());
+  Serial.print("  doubleclickThreshold is: ");  Serial.print(b.doubleclickThreshold);  Serial.println(" ms");
+
+  //we only want change one of the LEDs that corresponds to that button
+  if(b.pinValue()==b1Pin){ digitalWrite(led1Pin, !digitalRead(led1Pin)); }
+  if(b.pinValue()==b2Pin){ digitalWrite(led2Pin, !digitalRead(led2Pin)); }
+}
+
+/* ***********************************************************
+ *                         Void Setup                        *
+ * ********************************************************* */
 void setup(){
   Serial.begin(9600);
   Serial.println("Started");
-  pinMode(13,OUTPUT);              //debug to led 13
+  pinMode(led1Pin,OUTPUT);                  //LED1 on pin 11
+  pinMode(led2Pin,OUTPUT);                  //LED2 on pin 12
 
   // Assign callback functions
-  myButton1.pressHandler(onPress);
-  myButton1.releaseHandler(onRelease);
-  myButton1.holdHandler(onHold, 1000); // must be held for at least 1000 ms to trigger
+  ltButton.pressHandler(onPress);
+  ltButton.releaseHandler(onRelease);
+  ltButton.holdHandler(onHold, 1000); // must be held for at least 1000 ms to trigger
+  ltButton.doubleclickThreshold = 900;// double-click threshold set for 900 ms max to trigger
+  ltButton.doubleclickHandler(onDoubleClick);
 
-  myButton2.pressHandler(onPress);
-  myButton2.releaseHandler(onRelease);
-  myButton2.holdHandler(onHold, 1000); // must be held for at least 1000 ms to trigger
+  rtButton.pressHandler(onPress);
+  rtButton.releaseHandler(onRelease);
+  rtButton.holdHandler(onHold, 1000); // must be held for at least 1000 ms to trigger
+  rtButton.doubleclickThreshold = 900;// double-click threshold set for 900 ms max to trigger
+  rtButton.doubleclickHandler(onDoubleClick);
 }
 
+/* ***********************************************************
+ *                         Void Loop                         *
+ * ********************************************************* */
 void loop(){
   // update the buttons' internals
-  myButton1.process();
-  myButton2.process();
+  ltButton.process();
+  rtButton.process();
 }
