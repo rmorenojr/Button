7430290d8087a1c940f98086b35230813bee1179
rmorenojr <35234626+rmorenojr@users.noreply.github.com>
2018-09-15 22:22:42 -0600

    Revised example UniquePress.ino

--- Examples/UniquePress/UniquePress.ino
+++ Examples/UniquePress/UniquePress.ino
@@ -1,15 +1,18 @@
-/*
+/* ***********************************************************
 ||
 || @file UniquePress.ino
-|| @version 1.1
+|| @version 1.2
 || @author Ted Hayes
 || @contact ted.hayes@liminastudio.com
 ||
 || @description
-|| | Detects a state change in the button
+|| | Detects if the button a state change in the button
 || #
 ||
 || @revisions
+|| | 09_15_2018 v1.2 Revised Wiring, LED 13 used by serial monitor,
+|| |                 revised digitalWrite statement in void loop to toggle
+|| |                 by Ricardo Moreno
 || | 09_13_2018 v1.1 Correct file name above and revised instantiation
 || |                 object name and format by Ricardo Moreno 
 || #
@@ -18,28 +21,28 @@
 || | Copyright (c) 2012 Ted Hayes. All rights reserved.
 || #
 ||
-*/
+ * ********************************************************* */
 
 #include <Button.h>
 
-/*
+/* ***********************************************************
 || Wiring:
-|| GND -----/ ------ pin 12 to button
-|| GND -----/ ------ pin 13 to LED
-*/
-//create a Button object at pin 12
-Button button(12, BUTTON_PULLUP_INTERNAL);
+|| GND -----/ ------ pin 11 to button
+|| GND -----/ ------ pin 12 to LED
+||
+ * ********************************************************* */
+
+//create a Button object at pin 11
+Button button(11, BUTTON_PULLUP_INTERNAL);
 
 void setup(){
   Serial.begin(9600);
-  pinMode(13,OUTPUT);         //debug to led 13
+  pinMode(12,OUTPUT);         //led on pin 12
 }
 
 void loop(){
   if(button.uniquePress()){
     Serial.println("Button Pressed");
-    digitalWrite(13,HIGH);
-  }else{
-    digitalWrite(13,LOW);
+    digitalWrite(12, !digitalRead(12)); 
   }
 }
